6cadf44b413bfb182ccd8ce9cd3b4dfe621ab45f
Subhath Chathuranga <csubhath@gmail.com>
2024-05-09 09:16:14 +0530

    new changes

--- Product/shop_all/script.js
+++ Product/shop_all/script.js
@@ -113,7 +113,98 @@ filterBtn4.addEventListener("click", function () {
     btnClick4.classList.add("active");
     btnClick5.classList.add("active");
             
-    });
+});
     
 
+
+let fltrBtn1 = document.getElementById("close-1");
+
+fltrBtn1.addEventListener("click", function () {
+    
+    let btnClk1 = document.querySelector(".filterBox-1");
+      
+    btnClk1.classList.add("active");
+      
+});
+
+let fltrBtn2 = document.getElementById("close-2");
+
+fltrBtn2.addEventListener("click", function () {
+        
+        let btnClk2 = document.querySelector(".filterBox-2");
+          
+        btnClk2.classList.add("active");
+          
+});
+
+let fltrBtn3 = document.getElementById("close-3");
+
+fltrBtn3.addEventListener("click", function () {
+    
+    let btnClk3 = document.querySelector(".filterBox-3");
+      
+    btnClk3.classList.add("active");
+      
+});
+
+let fltrBtn4 = document.getElementById("close-4");
+
+fltrBtn4.addEventListener("click", function () {
+        
+        let btnClk4 = document.querySelector(".filterBox-4");
+          
+        btnClk4.classList.add("active");
+          
+});
+
+
+let chkBtn1 = document.getElementById("chk-1");
+
+chkBtn1.addEventListener("click", function () {
+    
+    let btnClk1 = document.querySelector(".chk-clss-1");
+      
+    btnClk1.classList.toggle("active");
+      
+});
+
+let chkBtn2 = document.getElementById("chk-2");
+
+chkBtn2.addEventListener("click", function () {
+    
+    let btnClk2 = document.querySelector(".chk-clss-2");
+      
+    btnClk2.classList.toggle("active");
+      
+});
+
+let chkBtn3 = document.getElementById("chk-3");
+
+chkBtn3.addEventListener("click", function () {
+    
+    let btnClk3 = document.querySelector(".chk-clss-3");
+      
+    btnClk3.classList.toggle("active");
+      
+});
+
+let chkBtn4 = document.getElementById("chk-4");
+
+chkBtn4.addEventListener("click", function () {
+    
+    let btnClk4 = document.querySelector(".chk-clss-4");
+      
+    btnClk4.classList.toggle("active");
+      
+});
+
+let chkBtn5 = document.getElementById("chk-5");
+
+chkBtn5.addEventListener("click", function () {
+    
+    let btnClk5 = document.querySelector(".chk-clss-5");
+      
+    btnClk5.classList.toggle("active");
+      
+});
 
